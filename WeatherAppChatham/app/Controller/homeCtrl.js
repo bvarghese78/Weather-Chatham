da683ec042ef3257fc75c1b12914e4b1148972cc
Benji Varghese <benjivarghese@gmail.com>
2015-10-13 00:15:40 -0500

    Geolocation part 1

--- WeatherAppChatham/app/Controller/homeCtrl.js
+++ WeatherAppChatham/app/Controller/homeCtrl.js
@@ -2,6 +2,18 @@
 
     $scope.weatherSource = null;
 
+    $scope.showPosition = function (pos) {
+        $scope.lat = pos.coords.latitude;
+        $scope.lng = pos.coords.longitude;
+    }
+
+    $scope.showError = function (pos) {
+        $scope.lat1 = pos.coords.latitude;
+        $scope.lng1 = pos.coords.longitude;
+    }
+
+    navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
+
     $scope.navigateTo = function () {
         if ($scope.weatherSource == "forecastIO") {
             $location.path('/forecastio');
